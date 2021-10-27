6bd5eefe87346a9183b33bc4ed50ca4857baa0a2
s0mnaths <somnathsharmaji05@gmail.com>
2021-10-27 23:37:25 +0530

    Add path HTML tag as invert exception

--- appOff.js
+++ appOff.js
@@ -1,7 +1,7 @@
 (function(){
     document.querySelector("html").style.filter = "invert(0) hue-rotate(0deg)";
 
-    let media = document.querySelectorAll("img, picture, video");
+    let media = document.querySelectorAll("img, picture, video, path");
     
     media.forEach ((mediaItem) => {
         mediaItem.style.filter = "invert(0) hue-rotate(0deg)";
--- appOn.js
+++ appOn.js
@@ -1,7 +1,7 @@
 (function(){
     document.querySelector("html").style.filter = "invert(1) hue-rotate(180deg)";
 
-    let media = document.querySelectorAll("img, picture, video");
+    let media = document.querySelectorAll("img, picture, video, path");
     
     media.forEach ((mediaItem) => {
         mediaItem.style.filter = "invert(1) hue-rotate(180deg)";
